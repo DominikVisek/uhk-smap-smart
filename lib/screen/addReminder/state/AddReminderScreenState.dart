6adddb5ee263cc927acc2f429f2a43110d1fa785
Dominik Víšek <visekdo@gmail.com>
2019-12-04 22:04:12 +0100

    Init of newReminderFormWidget

--- lib/screen/addReminder/state/AddReminderScreenState.dart
+++ lib/screen/addReminder/state/AddReminderScreenState.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:uhk_smap_smart_weather_reminder/screen/addReminder/AddReminderScreen.dart';
+import 'package:uhk_smap_smart_weather_reminder/widget/reminder/newReminderForm.dart';
 
 class AddReminderScreenState extends State<AddReminderScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
@@ -16,29 +17,22 @@ class AddReminderScreenState extends State<AddReminderScreen> {
           (MediaQuery.of(context).orientation == Orientation.portrait ? 2 : 3);
     });
 
-    final rightContent = Expanded(
-      flex: 1,
-      child: Container(
-        padding: EdgeInsets.fromLTRB(0, 15.0, 15.0, 15.0),
-      ),
-    );
-
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Text(
-            'New reminder',
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-            ),
+        backgroundColor: Colors.black,
+        title: Text(
+          'New reminder',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
           ),
+        ),
       ),
       body: Container(
         color: Colors.black12.withOpacity(0.06),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[rightContent],
+          children: <Widget>[NewReminderForm()],
         ),
       ),
     );
---
+++ lib/widget/reminder/newReminderForm.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class NewReminderForm extends StatefulWidget {
+  @override
+  NewReminderFormState createState() {
+    return NewReminderFormState();
+  }
+}
+
+class NewReminderFormState extends State<NewReminderForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
